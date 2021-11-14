f56d50f05d58bcba3a00c2cf8c317a9ca1b7d4b1
31Chiu <92836106+31Chiu@users.noreply.github.com>
2021-11-14 13:15:46 +0800

    Add files via upload

--- database/food_delivery_platform.sql
+++ database/food_delivery_platform.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 09, 2021 at 09:55 AM
+-- Generation Time: Nov 14, 2021 at 06:15 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.3.30
 
@@ -38,7 +38,7 @@ CREATE TABLE `admin_login` (
 --
 
 INSERT INTO `admin_login` (`admin_id`, `admin_email`, `admin_password`) VALUES
-(1, 'siewchinn1031@gmail.com', '378');
+(1, 'siewchinn1031@gmail.com', '123');
 
 -- --------------------------------------------------------
 
@@ -50,7 +50,7 @@ CREATE TABLE `customer_data` (
   `cust_id` int(11) NOT NULL,
   `cust_name` varchar(25) NOT NULL,
   `cust_email` varchar(25) NOT NULL,
-  `cust_phoneNo` int(10) NOT NULL,
+  `cust_phoneNo` bigint(12) NOT NULL,
   `cust_address` longtext NOT NULL,
   `cust_password` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -60,11 +60,24 @@ CREATE TABLE `customer_data` (
 --
 
 INSERT INTO `customer_data` (`cust_id`, `cust_name`, `cust_email`, `cust_phoneNo`, `cust_address`, `cust_password`) VALUES
-(1, 'chiu', 'd@gmail.com', 123456789, '1', '123'),
-(13, 'Fire', 'chinchin@gmail.com', 123456789, 'No.68, Lorong 3, Jalan Chung Nik, 96100 Sarikei, Sarawak.', '1'),
-(19, 'Lim', 'yoyomancomeon@gmail.com', 178599238, 'Miri', 'lim'),
-(21, 'Lim', 'limjialeong1281@gmail.com', 1234567890, '7878ssssss', '12'),
-(22, 'CSC', 'siewchinn1031@gmail.com', 178599238, 'No.68sasdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', '23');
+(21, 'Lim', 'limjialeong1281@gmail.com', 1234567890, '7878ssssss', 'lim'),
+(40, 'jj', 'f@gmail.com', 60178599238, 'aaa', 'fff'),
+(41, 'Lim', 'yoyomancomeon@gmail.com', 601110569319, 'dadad', '55'),
+(42, 'Chiu', 'siewchinn1031@gmail.com', 60178599238, 'No.68 Lorong 3, Jalan Chung NIk, 96100 Sarikei Sarawak.', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item`
+--
+
+CREATE TABLE `item` (
+  `item_id` int(11) NOT NULL,
+  `item_ing` varchar(256) NOT NULL,
+  `item_name` varchar(256) NOT NULL,
+  `item_type` varchar(30) NOT NULL,
+  `item_pr` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -97,6 +110,12 @@ ALTER TABLE `customer_data`
   ADD UNIQUE KEY `customer_email` (`cust_email`);
 
 --
+-- Indexes for table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`item_id`);
+
+--
 -- Indexes for table `reset_password`
 --
 ALTER TABLE `reset_password`
@@ -116,13 +135,19 @@ ALTER TABLE `admin_login`
 -- AUTO_INCREMENT for table `customer_data`
 --
 ALTER TABLE `customer_data`
-  MODIFY `cust_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `cust_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- AUTO_INCREMENT for table `item`
+--
+ALTER TABLE `item`
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `reset_password`
 --
 ALTER TABLE `reset_password`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
